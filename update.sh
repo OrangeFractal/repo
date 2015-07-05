c7152689c1be22c673b4ca4b41580524b66c079b
Sean <orangefractal@gmail.com>
2015-07-05 13:53:04 -0500

    update2

--- update.sh
+++ update.sh
@@ -1,5 +1,4 @@
 ./remove.sh
-<<<<<<< HEAD
 
 dpkg-deb -b ArcadeKeypad
 dpkg-deb -b LCDKeypad
@@ -14,9 +13,4 @@ dpkg-deb -b Snipe
 dpkg-deb -b ZeppelinSonictheHedgehog
 
 dpkg-scanpackages . /dev/null >Packages
-=======
-./packages.sh
-
-dpkg-scanpackages -m . /dev/null >Packages
->>>>>>> 91201222d320763e0a26a82277abe174c2e80cba
 bzip2 Packages
